4a671bb372c7a1d9e66d194699a2420e033a2043
Myo Min Htoo <myominhtoo2003@gmail.com>
2022-09-06 21:55:02 +0630

    added routes

--- src/main/resources/views/src/app/app-routing.module.ts
+++ src/main/resources/views/src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import AddBookComponent from './components/add-book/add-book.component';
+import BooksComponent from './components/books/books.component';
+import HomeComponent from './components/home/home.component';
+import UpdateBookComponent from './components/update-book/update-book.component';
 
 const routes: Routes = [
-
+  {
+    path : '',
+    component : HomeComponent,
+  },
+  {
+    path : 'books',
+    component : BooksComponent,
+  },
+  {
+     path : 'books/:id',
+     component : UpdateBookComponent,
+  },
+  {
+    path : "addbook",
+    component : AddBookComponent
+  }
 ];
 
 @NgModule({
--- src/main/resources/views/src/app/app.module.ts
+++ src/main/resources/views/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import AddBookComponent from './components/add-book/add-book.component';
+import BooksComponent from './components/books/books.component';
+import HomeComponent from './components/home/home.component';
+import UpdateBookComponent from './components/update-book/update-book.component';
+import NavBarComponent from './layouts/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookComponent,
+    BooksComponent,
+    UpdateBookComponent,
+    HomeComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
